6b5d808ac961336709577eca6f6df1f00fbc222d
Hugo Gutiérrez Tomás <uo277891@uniovi.es>
2022-03-31 18:59:57 +0200

    Detalles separados a una clase

--- webapp/src/pages/ProductoLista.tsx
+++ webapp/src/pages/ProductoLista.tsx
@@ -10,79 +10,13 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import pinchoEstatico from '../images/pincho2.jpg';
-import cabecera from '../images/cabecera.jpg';
 import Avatar from '@mui/material/Avatar';
 import picante from '../images/picante.png';
 import vegetariano from '../images/vegetariano.jpg';
 import Tooltip from '@mui/material/Tooltip';
-import InfoIcon from '@mui/icons-material/Info';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Divider from '@mui/material/Divider';
-
-type Anchor = 'top' | 'left' | 'bottom' | 'right';
+import Detalles from '../components/DetallesProducto';
 
 function ProductoLista(props:any){
-
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
-
-const toggleDrawer =
-  (anchor: Anchor, open: boolean) =>
-  (event: React.KeyboardEvent | React.MouseEvent) => {
-    if (
-      event.type === 'keydown' &&
-      ((event as React.KeyboardEvent).key === 'Tab' ||
-        (event as React.KeyboardEvent).key === 'Shift')
-    ) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
-
-const list = (anchor: Anchor) => (
-  
-  <Box
-    sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-    role="presentation"
-    onClick={toggleDrawer(anchor, false)}
-    onKeyDown={toggleDrawer(anchor, false)}
-  >
-    <img id = "detalles" src={cabecera}/>
-      <Typography textAlign="center" gutterBottom variant="h2" component="div">
-          {props.product.nombre}
-        </Typography>
-        <Divider></Divider>
-        <Typography textAlign="center" gutterBottom variant="h4" color = "#000034" component="div">
-            Descripción:
-        </Typography>
-        <Typography textAlign="center" gutterBottom variant="h5" component="div">
-          Pincho de pollo de corral, acompañado de lechuga fresca y pan.
-        </Typography>
-        <Typography textAlign="center" gutterBottom variant="h4" color = "#000034" component="div">
-            Ingredientes:
-        </Typography>
-        <Typography textAlign="center" gutterBottom variant="h5" component="div">
-            - Pan
-        </Typography>
-        <Typography textAlign="center" gutterBottom variant="h5" component="div">
-            - Pollo
-        </Typography>
-        <Typography textAlign="center" gutterBottom variant="h5" component="div">
-            - Lechuga
-        </Typography>
-        <Divider></Divider>
-        <Typography textAlign="center" variant="body2" color="text.secondary">
-          Precio: {props.product.precio} €
-        </Typography>
-    
-  </Box>
-);
 
     const {addToCart, increase, cartState} = useContext(CartContext);
 
@@ -148,19 +82,7 @@ const list = (anchor: Anchor) => (
           isInCart(props.product) &&
         <Button variant="contained" sx={{color: '#fff', m:1, backgroundColor: '#596886'}} endIcon={<AddCircleIcon />} onClick= {() => handleIncrease(props.product)} >Añadir más</Button>
         }
-        <div>
-        <React.Fragment key={'top'}>
-        <Button variant="contained" sx={{color: '#fff', m:1, backgroundColor: '#581845'}} endIcon={<InfoIcon />} onClick={toggleDrawer('top', true)} >Detalles producto</Button>
-          <Drawer
-            anchor={'top'}
-            open={state['top']}
-            onClose={toggleDrawer('top', false)}
-            variant='temporary'
-          >
-            {list('top')}
-          </Drawer>
-        </React.Fragment>
-        </div>
+        <Detalles product = {props.product}></Detalles>
       </CardActions>
     </Card>
     </Grid>
