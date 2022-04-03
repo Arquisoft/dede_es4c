f7c5ced2fbddea00d8550082f3ecef8aabbdfc09
uo277603 <uo277603@uniovi.es>
2022-04-03 12:52:45 +0200

    Commit para poder mergear

--- webapp/src/components/CartDrawer.tsx
+++ webapp/src/components/CartDrawer.tsx
@@ -4,11 +4,6 @@ import Drawer from '@mui/material/Drawer';
 
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import IconButton from '@mui/material/IconButton';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
