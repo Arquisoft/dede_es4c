7eee924f7a676ef9943f5b44b3d86bffa52c6cc0
uo277603 <uo277603@uniovi.es>
2022-04-29 18:26:00 +0200

    Cambiado el .env por el string de conexion

--- restapi/config/bd.ts
+++ restapi/config/bd.ts
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const connectionString = process.env.MONGO_DB;
 
 const connectBD = async () => {
-    mongoose.connect(connectionString)
+    mongoose.connect("mongodb+srv://es4c:es4c@cluster0.hcz1f.mongodb.net/bar_pinchos?retryWrites=true&w=majority")
     .then(() => {
         console.log("Database connected")
     }).catch(()=>{
