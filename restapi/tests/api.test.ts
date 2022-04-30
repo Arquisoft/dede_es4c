9fd0609bf22f29e3ae8a6f77c59b737fc7f70c9c
uo277306 <uo277306@uniovi.es>
2022-04-30 18:59:07 +0200

    Debug

--- restapi/tests/api.test.ts
+++ restapi/tests/api.test.ts
@@ -103,8 +103,8 @@ describe('user', () => {
         // Login an existent user
         let user = { "email": "dani@gmail.com", "password": process.env.PASSWD1 }
 
-        console.log("PASSWORD1: " + process.env.PASSWD1);
-        console.log("PASSWORD2: " + process.env.PASSWD2);
+        console.log("PASSWORD1: " + process.env.PASSWD1?.toString());
+        console.log("PASSWORD2: " + process.env.PASSWD2?.toString());
 
         var response: Response = await request(app).post('/api/login').send(user).set('Accept', 'application/json');
         expect(response.statusCode).toBe(200);
