a5b21a91d148a1217caf69bec39a652a4760ee8f
Hugo Gutiérrez Tomás <uo277891@uniovi.es>
2022-05-01 17:44:41 +0200

    test config v5

--- webapp/src/tests/jest.config.ts
+++ webapp/src/tests/jest.config.ts
@@ -4,7 +4,7 @@ export default {
         "^.+\\.tsx?$": "ts-jest"
     },
     collectCoverage: true,
-    collectCoverageFrom:["/components/*.tsx"],
+    collectCoverageFrom:["/components/*"],
     testTimeout:15000
 
 }
