2683e914e4514763203119722878089821850598
Hugo Gutiérrez Tomás <uo277891@uniovi.es>
2022-05-01 18:40:04 +0200

    test config v12

--- webapp/jest.config.ts
+++ webapp/jest.config.ts
@@ -2,6 +2,7 @@ export default {
     transform: {
         "^.+\\.tsx?$": "ts-jest"
     },
+    testMatch: ["<rootDir>/tests/*.tsx"],
     collectCoverage: true,
-    collectCoverageFrom:["**/*.tsx"],
+    collectCoverageFrom:["**/*.tsx"]
 }
