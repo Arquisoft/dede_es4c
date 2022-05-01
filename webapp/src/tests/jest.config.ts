e0906fba134ba31d55ace279e5cc2ee93d23502f
Hugo Gutiérrez Tomás <uo277891@uniovi.es>
2022-05-01 19:15:21 +0200

    v18

--- webapp/src/tests/jest.config.ts
+++ webapp/src/tests/jest.config.ts
@@ -1,8 +1,7 @@
 export default {
-    rootDir: './../',
     transform: {
         "^.+\\.tsx?$": "ts-jest"
     },
     collectCoverage: true,
-    collectCoverageFrom:["components/*.tsx"]
+    collectCoverageFrom:["/src/components/*.tsx"]
 }
