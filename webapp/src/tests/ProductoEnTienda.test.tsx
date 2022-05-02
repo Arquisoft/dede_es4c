5097df632ccaa60afc6739907c069aada9fd0484
Hugo Gutiérrez Tomás <uo277891@uniovi.es>
2022-05-02 22:58:10 +0200

    prueba

--- webapp/src/tests/ProductoEnTienda.test.tsx
+++ webapp/src/tests/ProductoEnTienda.test.tsx
@@ -10,7 +10,7 @@ test('check with any product', async () => {
 
 test('check with a product', async () => {
     const { getByText } = render(<Product nombre = "Lomo" precio = {3}/>);
-    expect(getByText('Lomo')).toBeInTheDocument();
+    expect(getByText('Lomo')).toBeInTheDocument(); 
     expect(getByText('Precio: 3 €')).toBeInTheDocument();
     expect(getByText('Añadir al carrito')).toBeInTheDocument();
 })
