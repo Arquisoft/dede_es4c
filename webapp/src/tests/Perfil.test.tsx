da8ec64ca72c154ce80876b4c186f7d8c9cb53f2
Hugo Gutiérrez Tomás <uo277891@uniovi.es>
2022-05-02 12:29:45 +0200

    otra prueba mas

--- webapp/src/tests/Perfil.test.tsx
+++ webapp/src/tests/Perfil.test.tsx
@@ -4,10 +4,9 @@ import React from "react";
 import { UserProvider } from "../context/userProvider";
 import Perfil from "../pages/Perfil";
 
-
 test('check the profile page', async () => {
     const { getByText } = render(<SessionProvider><UserProvider><Perfil/></UserProvider></SessionProvider>);
     expect(getByText('POD no vinculado')).toBeInTheDocument();
     expect(getByText('Por favor, conéctate con tu POD para que podamos utilizar tus datos')).toBeInTheDocument();
-    expect(getByText('Entra con tu POD')).toBeInTheDocument();
+    expect(getByText('Entra con tu POD')).toBeInTheDocument(); 
 })
