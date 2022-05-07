16a31da3a61011d0d34e93aba7197a6ec2da700d
uo277603 <uo277603@uniovi.es>
2022-05-07 14:36:28 +0200

    https restapi

--- restapi/server.ts
+++ restapi/server.ts
@@ -6,12 +6,24 @@ const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const memberRoutes = require('./routes/memberRoutes');
+var http = require('http');
+
 
 const bd = require('./config/bd');
 bd.connectBD();
 
 const app: Application = express();
 const port: number = 5000;
+
+let fs = require('fs');
+let https = require('https');
+let privateKey = fs.readFileSync('cert/key-20220507-140239.pem', 'utf8');
+let certificate = fs.readFileSync('cert/cert-20220507-140239.crt', 'utf8');
+let credentials = {key: privateKey, cert: certificate};
+
+let server = http.createServer(app);
+let httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port);
 
 const metricsMiddleware: RequestHandler = promBundle({ includeMethod: true });
 app.use(metricsMiddleware);
@@ -26,7 +38,7 @@ app.use('/api', orderRoutes);
 app.use('/api', memberRoutes);
 //app.use("/api", api)
 
-app.listen(port, (): void => {
+server.listen(app.get('port'), (): void => {
   console.log('Restapi listening on ' + port);
 }).on("error", (error: Error) => {
   console.error('Error occured: ' + error.message);
