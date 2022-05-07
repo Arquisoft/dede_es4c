b497bfdc6bc59e9123cd2563f0e34f1ad61e3111
uo277603 <uo277603@uniovi.es>
2022-05-07 14:16:37 +0200

    Prueba

--- webapp/src/pages/Perfil.tsx
+++ webapp/src/pages/Perfil.tsx
@@ -34,7 +34,7 @@ const Perfil = () => {
 	const [country, setCountry] = useState("");
 
 	const idp = "https://broker.pod.inrupt.com";
-	const redirUrl = "http://localhost:3000/Perfil";
+	const redirUrl = "https://dede-es4c.centralus.cloudapp.azure.com:3000//Perfil";
 
 	useEffect(() => {
 		handleIncomingRedirect({
